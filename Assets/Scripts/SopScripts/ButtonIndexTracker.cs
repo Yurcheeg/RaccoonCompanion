4595b3e3f8e8d8234c95e2408ea2d06f2d8a8796
HosTawY <152405636+HosTawY@users.noreply.github.com>
2024-06-08 12:17:46 +0300

    added backgrounds to shop

--- Assets/Scripts/SopScripts/ButtonIndexTracker.cs
+++ Assets/Scripts/SopScripts/ButtonIndexTracker.cs
@@ -4,36 +4,21 @@ using UnityEngine.UI;
 public class ButtonIndexTracker : MonoBehaviour
 {
     public Button[] buttons; // Массив кнопок для выбора фона
+    
+    //public Image imagge;
     public Button changeBackgroundButton; // Кнопка для изменения фона
     public BackgroundChanger backgroundChanger; // Скрипт для изменения фона
     private int selectedBackgroundIndex = -1; // Выбранный индекс фона
 
-    void Start()
+    void Awake()
     {
-        if (buttons == null || buttons.Length == 0)
-        {
-            Debug.LogError("Buttons array is not assigned or empty.");
-            return;
-        }
-
-        if (changeBackgroundButton == null)
-        {
-            Debug.LogError("ChangeBackgroundButton is not assigned.");
-            return;
-        }
-
-        if (backgroundChanger == null)
-        {
-            Debug.LogError("BackgroundChanger is not assigned.");
-            return;
-        }
-
-        // Добавляем слушатели к каждой кнопке выбора фона
+        
         for (int i = 0; i < buttons.Length; i++)
         {
             int index = i; // Локальная переменная для замыкания
             if (buttons[i] != null)
             {
+                buttons[i].image.sprite = backgroundChanger.backgroundSprites[i];
                 buttons[i].onClick.AddListener(() => OnButtonClicked(index));
             }
             else
@@ -41,6 +26,12 @@ public class ButtonIndexTracker : MonoBehaviour
                 Debug.LogWarning($"Button at index {i} is not assigned.");
             }
         }
+    }
+    void Start()
+    {
+        
+        // Добавляем слушатели к каждой кнопке выбора фона
+        
 
         // Прячем кнопку изменения фона
         changeBackgroundButton.gameObject.SetActive(true);
@@ -50,9 +41,7 @@ public class ButtonIndexTracker : MonoBehaviour
     void OnButtonClicked(int buttonIndex)
     {
         selectedBackgroundIndex = buttonIndex; // Запоминаем индекс нажатой кнопки
-        Debug.Log($"Button with index {buttonIndex} was clicked");
 
-        // Показываем кнопку изменения фона
         changeBackgroundButton.gameObject.SetActive(true);
     }
 
